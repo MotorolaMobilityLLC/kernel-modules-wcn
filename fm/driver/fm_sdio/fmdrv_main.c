ff98af16348aabc41bcea3bd37220d07c5a7428d
xiaopengGao <xiaopeng.gao@spreadtrum.com>
2019-08-27 15:49:18 +0800

    Bug #1150954 androidq: [SPD][monkey][android10.0_sharkl5pro_ctcc]monkey appear exception
    [root cause  ] fm driver rds wait handle affect hidl rds thread
    [changes     ] not use fm driver rds wait handle
    [side effects]no
    [self test   ]:ok
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:ums512_1h10
    [test case]:monkey test
    [reviewers   ] quan.li
    [change_type ] feature
    [tag_product ] common
    
    Change-Id: Ic1715ad3b87508e955e7960d59bfbb3893e723ac

--- fm/driver/fm_sdio/fmdrv_main.c
+++ fm/driver/fm_sdio/fmdrv_main.c
@@ -303,7 +303,6 @@ static void receive_tasklet(unsigned long arg)
 
 ssize_t fm_read_rds_data(struct file *filp, char __user *buf, size_t count, loff_t *pos) {
     int timeout = -1;
-    int ret;
     pr_info("(FM_RDS) fm start to read RDS data\n");
 #ifdef RDS_DEBUG
     sprintf(rds_debug_data.ps_data.PS[3], "PS_debug");
@@ -327,15 +326,6 @@ ssize_t fm_read_rds_data(struct file *filp, char __user *buf, size_t count, loff
         timeout = 0;
         pr_err("fm_read_rds_data NON BLOCK!!!\n");
         return -EWOULDBLOCK;
-    }
-
-    if (timeout < 0) {
-        /* wait forever */
-        ret = wait_event_interruptible((fmdev->rds_han.rx_queue), ((fmdev->rds_han.new_data_flag) == 1));
-        if (ret) {
-            pr_err("(FM RDS)wait_event_interruptible ret=%d\n", ret);
-            return -EINTR;
-        }
     }
 
     fmdev->rds_data.rt_data.textlength = strlen(fmdev->rds_data.rt_data.textdata[3]);
