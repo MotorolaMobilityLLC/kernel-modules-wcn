99c6513d34bcb4b69902495c0a3d89eb4794dec0
hongwei.cao <hongwei.cao@spreadtrum.com>
2019-12-03 20:51:57 +0800

    Bug #1192162 wcn: sc2332: modify tcp rx npi used by driver
    [root cause ] netif_rx_ni() caused tcp out of order
    [changes ] modify netif_rx_ni() to netif_receive_skb()
    [side effects] no
    [self test ]:no unorder tcp pkt
    [change_type] modify npi tcp rx
    [download normally]: Y
    [power on/off normally]:Y
    [do common repository/branch inspection]:NA
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:9863a
    [test case]:sta tcp rx
    [reviewers ] hongwei.cao beolei.yuan
    
    Change-Id: I5ccdc06dba2c1b7a8516191c238dcdbdf6797eb5
    Signed-off-by: hongwei.cao <hongwei.cao@spreadtrum.com>

--- wlan/sc2332/main.c
+++ wlan/sc2332/main.c
@@ -71,7 +71,10 @@ void sprdwl_netif_rx(struct sk_buff *skb, struct net_device *ndev)
 	ndev->stats.rx_packets++;
 	ndev->stats.rx_bytes += skb->len;
 
-	netif_rx_ni(skb);
+	//netif_rx_ni(skb);
+	local_bh_disable();
+	netif_receive_skb(skb);
+	local_bh_enable();
 }
 
 void sprdwl_stop_net(struct sprdwl_vif *vif)
