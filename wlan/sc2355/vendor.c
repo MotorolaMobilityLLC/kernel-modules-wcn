f93da81e90fcc9cc4d20c6ac6644a1ffa2873bc1
baolei.yuan <baolei.yuan@spreadtrum.com>
2020-06-23 16:26:46 +0800

    Bug #1345247 wifi: modify llstate return value
    [root cause  ] modify llstate return value
    [changes     ] wifi driver
    [side effects] no side effects
    [self test   ]: test wifi
    [download normally]: yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]: no
    [confirm dependent commit]:yes
    [board]:sharkl5 pro
    [test case]:test wifi
    Test: test wifi
    [reviewers   ] baolei.yuan
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Signed-off-by: baolei.yuan <baolei.yuan@spreadtrum.com>
    (cherry picked from commit fc7740dce1ef7f6028baa90cf4ed5bb967301b54)
    
    Change-Id: Id58a60c83f16e2f1c42f5ad157c7a12449fb8d6d

--- wlan/sc2355/vendor.c
+++ wlan/sc2355/vendor.c
@@ -450,9 +450,9 @@ static int sprdwl_vendor_get_llstat_handler(struct wiphy *wiphy,
 	struct wifi_iface_stat *iface_st;
 	struct sprdwl_llstat_radio *dif_radio;
 	u16 r_len = sizeof(*llst);
-	u8 r_buf[r_len], ret, i;
+	u8 r_buf[r_len], i;
 	u32 reply_radio_length, reply_iface_length;
-
+	int ret;
 	struct sprdwl_priv *priv = wiphy_priv(wiphy);
 	struct sprdwl_vif *vif = container_of(wdev, struct sprdwl_vif, wdev);
 
