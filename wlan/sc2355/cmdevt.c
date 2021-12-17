2062914070f88684bfaf56e941ac0394300df2df
wuji.han <wuji.han@chino-e.com>
2021-12-17 14:30:55 +0800

    Revert "moto_e32-11: Open 5G WLAN [1/2]"
    This reverts commit 9c4b2e9e8fd9fd7aa80e17f090875d9cbdc3e3da.
    
    Change-Id: I6eb4c704ad58e327180ea56a80af70d8dfe8e437

--- wlan/sc2355/cmdevt.c
+++ wlan/sc2355/cmdevt.c
@@ -860,6 +860,7 @@ int sprdwl_get_fw_info(struct sprdwl_priv *priv)
 		priv->chip_ver = p->chip_version;
 		priv->fw_ver = p->fw_version;
 		priv->fw_capa = p->fw_capa;
+		priv->fw_capa &= ~(1 << 0);
 		priv->fw_std = p->fw_std;
 		priv->extend_feature = p->extend_feature;
 		priv->max_ap_assoc_sta = p->max_ap_assoc_sta;
