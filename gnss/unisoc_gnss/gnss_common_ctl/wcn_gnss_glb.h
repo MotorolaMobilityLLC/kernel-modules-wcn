cef9fa894b67fe437c314337b87120dce6e7bf09
Cheng Liu <Cheng.Liu3@unisoc.com>
2023-04-11 19:29:57 +0800

    Bug #2216808 gnss: move gnss driver to new directory
    [root cause  ]:implementing a common repository of wcn code
    [changes     ]:all gnss drvier code
    [side effects]:gnss
    [self test   ]:gnss common test
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:yes
    [confirm dependent commit]:yes
    [board]:9863
    [test case]:gnss common test
    [reviewers]:andy.huang
    
    commit_template_version:v1
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: I83c500930931cdfb45c7fbba754e9e1de15d37a5
    Signed-off-by: Cheng Liu <Cheng.Liu3@unisoc.com>

---
+++ gnss/unisoc_gnss/gnss_common_ctl/wcn_gnss_glb.h
@@ -0,0 +1,24 @@
+/*
+ *
+ * SPDX-FileCopyrightText: 2021-2023 Unisoc (Shanghai) Technologies Co. Ltd
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Filename : wcn_gnss_glb.h
+ * Abstract : This file is a implementation for driver of gnss
+ *
+ * Authors  : zhaohui.chen  cheng.liu
+ *
+ */
+#ifndef __WCN_GNSS_GLB_H__
+#define __WCN_GNSS_GLB_H__
+
+extern uint GNSS_CP_START_ADDR;
+extern uint GNSS_FIRMWARE_MAX_SIZE;
+
+extern uint GNSS_DUMP_PACKET_SIZE;
+extern uint GNSS_SHARE_MEMORY_SIZE;
+extern uint GNSS_DUMP_IRAM_START_ADDR;
+extern uint GNSS_CP_IRAM_DATA_NUM;
+extern uint GNSS_DUMP_REG_NUMBER;
+
+#endif
