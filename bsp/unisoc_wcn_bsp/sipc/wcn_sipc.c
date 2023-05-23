6bf99d217f243218f554d9d8c63dae021aa4dc0a
Zhilong Chen <zhilong.chen@unisoc.com>
2023-05-23 17:03:42 +0800

    Bug #2259811 unisoc_platform: sprdwcn: adjust 3-4 notify ready when poweron
    [root cause  ]: adjust notify ready
    [changes     ]:sipc
    [side effects]:no
    [self test   ]:no
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:no
    [is there dependence]:yes
    [confirm dependent commit]:no
    [board]:ums9230
    [test case]:pass
    [reviewers]:yonghan.ye
    
    commit_template_version:v1
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I57ba5fc82795cf4fa5b91c4971956756a406cea0

--- bsp/unisoc_wcn_bsp/sipc/wcn_sipc.c
+++ bsp/unisoc_wcn_bsp/sipc/wcn_sipc.c
@@ -132,6 +132,16 @@ struct wcn_sipc_data_ops {
 	void (*sipc_push_list_dequeue)(struct sipc_chn_info *sipc_chn);
 };
 
+void wcn_sipc_chn_set_status(void *data, bool flag)
+{
+	struct sipc_chn_info *sipc_chn = (struct sipc_chn_info *)data;
+	WCN_INFO("wcn_sipc_chn_set_status chn: %d ,  flag:%d\n", sipc_chn->chn, flag);
+	if (flag)
+		sipc_chn->sipc_chn_status = true;
+	else
+		sipc_chn->sipc_chn_status = false;
+}
+
 struct wcn_sipc_data_ops  sipc_data_ops[] = {
 	{
 		.sipc_send = wcn_sipc_sbuf_push,
@@ -265,6 +275,11 @@ static void wcn_sipc_record_mbuf_recv_from_user(int index, int num)
 int wcn_sipc_channel_dir(int index)
 {
 	return g_sipc_chn[index].inout;
+}
+
+int wcn_sipc_chn_status(int index)
+{
+	return g_sipc_chn[index].sipc_chn_status;
 }
 
 struct sipc_chn_info *wcn_sipc_channel_get(int index)
@@ -550,6 +565,13 @@ static void wcn_sipc_sbuf_notifer(int event, void *data)
 			}
 		} while (cnt == buf_len);
 		break;
+	case SBUF_NOTIFY_READY:
+		WCN_INFO("sbuf index %d dts[%d] chn[%d] read event[%d]\n",
+						sipc_chn->index, sipc_chn->dst, sipc_chn->chn, event);
+		/*only set status index[1]:ATCMD*/
+		if (sipc_chn->index == SIPC_ATCMD_RX)
+			wcn_sipc_chn_set_status(sipc_chn, true);
+		break;
 	default:
 		WCN_ERR("sbuf read event[%d] invalid\n", event);
 	}
@@ -705,16 +727,6 @@ static void wcn_sipc_sblk_recv(struct sipc_chn_info *sipc_chn)
 			WCN_ERR("release sblock[%d] err:%d\n",
 				sipc_chn->chn, ret);
 	}
-}
-
-void wcn_sipc_chn_set_status(void *data, bool flag)
-{
-	struct sipc_chn_info *sipc_chn = (struct sipc_chn_info *)data;
-	WCN_INFO("wcn_sipc_chn_set_status chn: %d ,  flag:%d\n", sipc_chn->chn, flag);
-	if (flag)
-		sipc_chn->sipc_chn_status = true;
-	else
-		sipc_chn->sipc_chn_status = false;
 }
 
 void wcn_sipc_chn_set_status_all_false(void)
