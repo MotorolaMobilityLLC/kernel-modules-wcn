2bed88b9d43b0cc2bd787f5002e2055453746a5c
Yakun Lin <yakun.lin@unisoc.com>
2023-06-19 16:23:27 +0800

    Bug #2292605 wcn bsp: pcie scan card fail, call pcie_chn_init cause kernel crash
    [root cause  ]: pcie scan card fail, call pcie_chn_init cause kernel crash
    [changes     ]: wcn bsp
    [side effects]: no
    [self test   ]: yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: no
    [confirm dependent commit]: yes
    [board]: ums9610_1h10
    [test case]: N/A
    [reviewers]: yonghan.ye
    
    commit_template_version:v1
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I87b1c15480ae16c081728b5190812b25559eed52
    Signed-off-by: Yakun Lin <yakun.lin@unisoc.com>

--- bsp/unisoc_wcn_bsp/pcie/mchn.c
+++ bsp/unisoc_wcn_bsp/pcie/mchn.c
@@ -297,13 +297,19 @@ int mchn_init(struct mchn_ops_t *ops)
 	int ret = -1;
 	struct mchn_info_t *mchn = mchn_info();
 
+	if (ops == NULL) {
+		WCN_INFO("%s err, ops is null", __func__);
+		return -1;
+	}
+
 	WCN_DBG("[+]%s(chn=%d)\n", __func__, ops->channel);
-	if (ops->hif_type != HW_TYPE_PCIE) {
-		WCN_INFO("%s err, hif_type %d\n", __func__, ops->hif_type);
+	if (ops->hif_type != HW_TYPE_PCIE || !wcn_get_edma_status()) {
+		WCN_INFO("%s err, hif_type %d, chn=%d\n", __func__, ops->hif_type, ops->channel);
 		WARN_ON(1);
 
 		return -1;
 	}
+
 	mchn->ops[ops->channel] = ops;
 
 	switch (ops->hif_type) {
