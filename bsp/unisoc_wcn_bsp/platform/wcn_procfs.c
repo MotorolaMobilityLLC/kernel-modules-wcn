0e1431fbce78028ffa63f06f7e30a4f9bde47d79
fangqing.xi1 <fangqing.xi1@unisoc.com>
2023-06-27 09:47:57 +0800

    Bug #2300800 wifi hotspot sdio CMD52/53 timeout during wcn reset process
    [root cause  ]: ATcmd or other communications during reset process will course CMD52/53 error.
    		Previous A10 wcn reset process does match, use the A14 new one.
    		echo rebootwcn > /proc/mdbg/at_cmd
    [changes     ]: set sprdwcn_bus_set_carddump_status=true in wcn_reset_process
    [side effects]:na
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:no
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:ums512
    [test case]:pass
    [reviewers]:yonghan.ye
    
    Signed-off-by: fangqing.xi <fangqing.xi1@unisoc.com>
    commit_template_version:v1
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] specific
    
    Change-Id: Ieb127797deca1962b83bcec699f1c0b3827e9be4

--- bsp/unisoc_wcn_bsp/platform/wcn_procfs.c
+++ bsp/unisoc_wcn_bsp/platform/wcn_procfs.c
@@ -963,14 +963,8 @@ static ssize_t mdbg_proc_write(struct file *filp,
 		}
 		if (strncmp(mdbg_proc->write_buf, "rebootwcn", 9) == 0) {
 			flag_reset = 1;
-			WCN_INFO("marlin gnss need reset\n");
-			WCN_INFO("fail_count is value %d\n", mdbg_proc->fail_count);
 			stop_loopcheck();
-			mdbg_proc->fail_count = 0;
-			marlin_set_download_status(0);
-			sprdwcn_bus_set_carddump_status(false);
-			marlin_chip_en(false, true);
-			wcn_reset_cp2();
+			wcn_reset_process();
 			flag_reset = 0;
 			return count;
 		}
