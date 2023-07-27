28795f2eab6101afefda2b4f139da4e15637b304
Lun.Wang1 <Lun.Wang1@unisoc.com>
2023-07-27 08:28:06 +0800

    Bug #2333147 - ignore AP's countryIE when cfg80211_connect_result it
    [root cause  ]: ignore AP's countryIE when cfg80211_connect_result it
    [changes     ]:wifi driver
    [side effects]:no
    [self test   ]:pass
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:no
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:N6P
    [test case]:pass
    [reviewers]:xue.cai
    
    commit_template_version:v1
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: I7ee02f744253a3759270c96efcc24dfa15fd221d

--- wlan/wlan_combo/sc2355/init.c
+++ wlan/wlan_combo/sc2355/init.c
@@ -321,10 +321,9 @@ void sc2355_setup_wiphy(struct wiphy *wiphy, struct sprd_priv *priv)
 		wl_debug("\tIEEE802.11d supported\n");
 		wiphy->reg_notifier = sc2355_reg_notify;
 		wiphy->regulatory_flags |= REGULATORY_DISABLE_BEACON_HINTS;
-		if (priv->hif.hw_type == SPRD_HW_SC2355_SDIO) {
-			// ignore AP's countryIE when cfg80211_connect_result it
-			wiphy->regulatory_flags |= REGULATORY_COUNTRY_IE_IGNORE;
-		}
+
+		// ignore AP's countryIE when cfg80211_connect_result it
+		wiphy->regulatory_flags |= REGULATORY_COUNTRY_IE_IGNORE;
 	}
 
 	if (priv->fw_std & SPRD_STD_11E) {
