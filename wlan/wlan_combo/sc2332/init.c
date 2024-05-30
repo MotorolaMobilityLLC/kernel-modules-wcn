3e5ffc19b58df57c8f80199e8cba61c125a4c0c4
Juxiang Chen <juxiang.chen@unisoc.com>
2024-05-30 19:46:45 +0800

    Bug #2760010 wlan: set max_num_akm_suites to 3 to support cross akms roaming
    [root cause  ]: set max_num_akm_suites to 3 to support cross akms roaming
    [changes     ]:wlan
    [side effects]:none
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:none
    [is there dependence]:none
    [confirm dependent commit]:yes
    [board]:sprd
    [test case]:none
    [reviewers]:guohua.hu
    
    commit_template_version:v1
    
    Signed-off-by: Juxiang Chen <juxiang.chen@unisoc.com>
    (cherry picked from commit 288bcffff4a12dd12f56532a48eab663117afe1a)
    
    Change-Id: Ief96293f41d64e00f23425f692d37c41ffdd1cbf

--- wlan/wlan_combo/sc2332/init.c
+++ wlan/wlan_combo/sc2332/init.c
@@ -289,6 +289,12 @@ void sc2332_setup_wiphy(struct wiphy *wiphy, struct sprd_priv *priv)
 		wiphy_ext_feature_set(wiphy,
 				      NL80211_EXT_FEATURE_OCE_PROBE_REQ_HIGH_TX_RATE);
 	}
+
+	/*bug2674658:support cross akm roaming need set max_num_akm_suites to 3*/
+	if (priv->extend_feature & SPRD_EXTEND_CROSS_AKM_ROAMING) {
+		wl_debug("cross akm roaming supported\n");
+		wiphy->max_num_akm_suites = 3;
+	}
 }
 
 static const struct sprd_npi_ops sc2332_npi_ops[] = {
