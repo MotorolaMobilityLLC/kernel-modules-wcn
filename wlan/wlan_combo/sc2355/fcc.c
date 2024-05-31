444e8a2389de91231d2620686a05022ae127017c
yuran <ran.yu@chino-e.com>
2024-05-31 15:03:47 +0800

    EKmoto_g35-341:WiFi Power Levels based on country code.
    Description:
     -WiFi Power Levels based on country code.
    
    St-result: PASS
    
    Change-Id: I5b797426ad403169ab0fe78318a2a8db53b89fb8
    Signed-off-by: yuran <ran.yu@chino-e.com>
    Reviewed-on: https://gerrit-odm.mot.com/c/home/repo/dev/platform/android/unisoc/bsp/modules/wcn/+/681
    Tested-by: gerrito
    Reviewed-by: baile lv <baile.lv@chino-e.com>

--- wlan/wlan_combo/sc2355/fcc.c
+++ wlan/wlan_combo/sc2355/fcc.c
@@ -10,8 +10,35 @@
 /* subtype, channel, bw, {mode(2.4g : b,g,n,ac; 5g : a,n,ac), value} */
 
 #define num_ce 8
+#define num_fcc 14
 
-#define power_backoff_ce {{1, 149, 0, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 12}, {5, 12}, {6, 12} } }, {1, 153, 0, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 12}, {5, 12}, {6, 12} } }, {1, 157, 0, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 12}, {5, 12}, {6, 12} } }, {1, 161, 0, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 12}, {5, 12}, {6, 12} } }, {1, 165, 0, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 12}, {5, 12}, {6, 12} } }, {1, 151, 1, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 12}, {5, 12}, {6, 12} } }, {1, 159, 1, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 12}, {5, 12}, {6, 12} } }, {1, 155, 2, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 12}, {5, 12}, {6, 12} } }, }
+#define power_backoff_ce { \
+	{1, 149, 0, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 12}, {5, 12}, {6, 12} } }, \
+	{1, 153, 0, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 12}, {5, 12}, {6, 12} } }, \
+	{1, 157, 0, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 12}, {5, 12}, {6, 12} } }, \
+	{1, 161, 0, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 12}, {5, 12}, {6, 12} } }, \
+	{1, 165, 0, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 12}, {5, 12}, {6, 12} } }, \
+	{1, 151, 1, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 12}, {5, 12}, {6, 12} } }, \
+	{1, 159, 1, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 12}, {5, 12}, {6, 12} } }, \
+	{1, 155, 2, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 12}, {5, 12}, {6, 12} } }, \
+	}
+
+#define power_backoff_fcc { \
+	{1, 1, 0, { {0, 127}, {1, 127}, {2, 15}, {3, 127}, {4, 127}, {5, 127}, {6, 127} } },  \
+	{1, 11, 0, { {0, 127}, {1, 127}, {2, 14}, {3, 127}, {4, 127}, {5, 127}, {6, 127} } }, \
+	{1, 3, 1, { {0, 127}, {1, 127}, {2, 14}, {3, 127}, {4, 127}, {5, 127}, {6, 127} } }, \
+	{1, 9, 1, { {0, 127}, {1, 127}, {2, 13}, {3, 127}, {4, 127}, {5, 127}, {6, 127} } }, \
+	{1, 36, 0, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 15}, {5, 127}, {6, 127} } }, \
+	{1, 100, 0, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 15}, {5, 127}, {6, 127} } }, \
+	{1, 36, 0, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 127}, {5, 127}, {6, 15} } }, \
+	{1, 100, 0, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 127}, {5, 127}, {6, 15} } }, \
+	{1, 140, 0, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 127}, {5, 127}, {6, 10} } }, \
+	{1, 38, 1, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 127}, {5, 127}, {6, 12} } }, \
+	{1, 62, 1, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 127}, {5, 127}, {6, 12} } }, \
+	{1, 102, 1, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 127}, {5, 127}, {6, 13} } }, \
+	{1, 42, 2, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 127}, {5, 127}, {6, 12} } }, \
+	{1, 106, 2, { {0, 127}, {1, 127}, {2, 127}, {3, 127}, {4, 127}, {5, 127}, {6, 12} } }, \
+	}
 
 static struct sprd_fcc_priv fcc_info;
 
@@ -22,92 +49,7 @@ static struct fcc_power_bo g_fcc_power_table[MAX_FCC_COUNTRY_NUM] = {
 		.power_backoff = power_backoff_ce,
 	},
 	{
-		.country = "AL",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "AM",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "AN",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "AT",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "AW",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "AZ",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "BA",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "BD",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "BE",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "BG",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "BH",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "BL",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "BN",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "BO",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
 		.country = "BR",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "BY",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "BZ",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "CH",
 		.num = num_ce,
 		.power_backoff = power_backoff_ce,
 	},
@@ -122,27 +64,7 @@ static struct fcc_power_bo g_fcc_power_table[MAX_FCC_COUNTRY_NUM] = {
 		.power_backoff = power_backoff_ce,
 	},
 	{
-		.country = "CY",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "CZ",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
 		.country = "DE",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "DK",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "DZ",
 		.num = num_ce,
 		.power_backoff = power_backoff_ce,
 	},
@@ -152,22 +74,7 @@ static struct fcc_power_bo g_fcc_power_table[MAX_FCC_COUNTRY_NUM] = {
 		.power_backoff = power_backoff_ce,
 	},
 	{
-		.country = "EE",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "EG",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
 		.country = "ES",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "FI",
 		.num = num_ce,
 		.power_backoff = power_backoff_ce,
 	},
@@ -182,57 +89,7 @@ static struct fcc_power_bo g_fcc_power_table[MAX_FCC_COUNTRY_NUM] = {
 		.power_backoff = power_backoff_ce,
 	},
 	{
-		.country = "GE",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "GF",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "GL",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "GP",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "GR",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "HU",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "IE",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "IL",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
 		.country = "IN",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "IR",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "IS",
 		.num = num_ce,
 		.power_backoff = power_backoff_ce,
 	},
@@ -242,122 +99,7 @@ static struct fcc_power_bo g_fcc_power_table[MAX_FCC_COUNTRY_NUM] = {
 		.power_backoff = power_backoff_ce,
 	},
 	{
-		.country = "JO",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "KE",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "KH",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "KP",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "KR",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "KW",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "KZ",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "LB",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "LI",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "LK",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "LT",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "LU",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "LV",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "MA",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "MC",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "MK",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "MQ",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "MU",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "MW",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "MY",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "NG",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
 		.country = "NL",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "NO",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "NP",
 		.num = num_ce,
 		.power_backoff = power_backoff_ce,
 	},
@@ -367,37 +109,7 @@ static struct fcc_power_bo g_fcc_power_table[MAX_FCC_COUNTRY_NUM] = {
 		.power_backoff = power_backoff_ce,
 	},
 	{
-		.country = "PF",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "PG",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "PK",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
 		.country = "PL",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "PT",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "QA",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "RE",
 		.num = num_ce,
 		.power_backoff = power_backoff_ce,
 	},
@@ -412,11 +124,6 @@ static struct fcc_power_bo g_fcc_power_table[MAX_FCC_COUNTRY_NUM] = {
 		.power_backoff = power_backoff_ce,
 	},
 	{
-		.country = "RU",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
 		.country = "SA",
 		.num = num_ce,
 		.power_backoff = power_backoff_ce,
@@ -427,47 +134,7 @@ static struct fcc_power_bo g_fcc_power_table[MAX_FCC_COUNTRY_NUM] = {
 		.power_backoff = power_backoff_ce,
 	},
 	{
-		.country = "SG",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "SI",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "SK",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "SV",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "SY",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "TH",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
 		.country = "TN",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "TR",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
-		.country = "TT",
 		.num = num_ce,
 		.power_backoff = power_backoff_ce,
 	},
@@ -477,44 +144,44 @@ static struct fcc_power_bo g_fcc_power_table[MAX_FCC_COUNTRY_NUM] = {
 		.power_backoff = power_backoff_ce,
 	},
 	{
-		.country = "UG",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
-	},
-	{
 		.country = "UY",
 		.num = num_ce,
 		.power_backoff = power_backoff_ce,
 	},
 	{
-		.country = "VE",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
+		.country = "AR",
+		.num = num_fcc,
+		.power_backoff = power_backoff_fcc,
 	},
 	{
-		.country = "VN",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
+		.country = "AU",
+		.num = num_fcc,
+		.power_backoff = power_backoff_fcc,
 	},
 	{
-		.country = "YE",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
+		.country = "CO",
+		.num = num_fcc,
+		.power_backoff = power_backoff_fcc,
 	},
 	{
-		.country = "YT",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
+		.country = "DO",
+		.num = num_fcc,
+		.power_backoff = power_backoff_fcc,
 	},
 	{
-		.country = "ZA",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
+		.country = "GT",
+		.num = num_fcc,
+		.power_backoff = power_backoff_fcc,
 	},
 	{
-		.country = "ZW",
-		.num = num_ce,
-		.power_backoff = power_backoff_ce,
+		.country = "MX",
+		.num = num_fcc,
+		.power_backoff = power_backoff_fcc,
+	},
+	{
+		.country = "OM",
+		.num = num_fcc,
+		.power_backoff = power_backoff_fcc,
 	},
 };
 
